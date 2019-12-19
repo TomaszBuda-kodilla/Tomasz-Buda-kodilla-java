3e44b08e7024750c0fc947bbebc4fce4dd5fdfb5
leetdco2@gmail.com <leetdco2@gmail.com>
2019-12-19 16:19:42 +0100

    First unit test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,31 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        Calculator simpleCalculator = new Calculator(10,10);
+
+        int firstNumber = simpleCalculator.getA();
+        int secondNumber = simpleCalculator.getB();
+//TEST 1
+        if (firstNumber == 10 && secondNumber == 10 )
+        {
+            System.out.println(simpleCalculator.add(firstNumber,secondNumber) + " test OK");
+        }
+        else
+        {
+            System.out.println("Error!");
+        }
+//TEST 2
+        if (firstNumber == 10 && secondNumber == 10 )
+        {
+            System.out.println(simpleCalculator.subtract(firstNumber,secondNumber) + " test OK");
+        }
+        else
+        {
+            System.out.println("Error!");
+        }
+
     }
 }
