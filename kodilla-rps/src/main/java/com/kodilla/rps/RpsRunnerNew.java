09e4f5aa8ef68866325cbf48bff08dd0ff357c5c
leetdco2@gmail.com <leetdco2@gmail.com>
2020-01-27 07:28:00 +0100

    7.7 - random letter bug fix

--- kodilla-rps/src/main/java/com/kodilla/rps/RpsRunnerNew.java
+++ kodilla-rps/src/main/java/com/kodilla/rps/RpsRunnerNew.java
@@ -24,106 +24,111 @@ public class RpsRunnerNew {
             do {
                 System.out.println("Press  1-Scissors  |  2-Paper  |  3-Rock  |\n  x-End of the game  |  n-Restart the game");
                 String move = br.readLine();
-                switch (move) {
-                    case "x": {
-                        System.out.println("Do you really want to exit the game ? \n Press y-Yes or n-No");
-                        String decision = br.readLine();
-                        if (decision.equals("n")) {
-                            System.out.println("Welcome back to the game.");
-                        } else if (decision.equals("y")) {
-                            System.exit(0);
+                if (move.equals("1") || move.equals("2") || move.equals("3") || move.equals("x") || move.equals("n")) {
+                    switch (move) {
+                        case "x": {
+                            System.out.println("Do you really want to exit the game ? \n Press y-Yes or n-No");
+                            String decision = br.readLine();
+                            if (decision.equals("n")) {
+                                System.out.println("Welcome back to the game.");
+                            } else if (decision.equals("y")) {
+                                System.exit(0);
+                            } else {
+                                System.out.println("Wrong option");
+                            }
+                            break;
+                        }
+                        case "n": {
+                            System.out.println("Do you really want to restart the game ? \n Press y-Yes or n-No");
+                            String decision = br.readLine();
+                            if (decision.equals("n")) {
+                                System.out.println("Continue playing the game");
+                            } else if (decision.equals("y")) {
+                                game.restartRounds();
+                            } else {
+                                System.out.println("Wrong option");
+                            }
+                            break;
+                        }
+                        case "1": {
+                            System.out.println("You picked Scissors");
+                            game.subtractRound();
+                            switch (enemy.getEnemyMove()) {
+                                case "1":
+                                    System.out.println("Enemy choice is: Scissors");
+                                    user.addMyScore();
+                                    enemy.addEnemyScore();
+                                    break;
+                                case "2":
+                                    System.out.println("Enemy choice is: Paper");
+                                    user.addMyScore();
+                                    break;
+                                case "3":
+                                    System.out.println("Enemy choice is: Rock");
+                                    enemy.addEnemyScore();
+                                    break;
+                            }
+                            System.out.println("Score is: " + user.getMyScore() + " : " + enemy.getEnemyScore());
+                            break;
+                        }
+                        case "2": {
+                            System.out.println("You picked Paper");
+                            game.subtractRound();
+                            switch (enemy.getEnemyMove()) {
+                                case "2":
+                                    System.out.println("Enemy choice is Paper");
+                                    enemy.addEnemyScore();
+                                    user.addMyScore();
+                                    break;
+                                case "1":
+                                    System.out.println("Enemy choice is Scissors");
+                                    enemy.addEnemyScore();
+                                    break;
+                                case "3":
+                                    System.out.println("Enemy choice is Rock");
+                                    user.addMyScore();
+                                    break;
+                            }
+                            System.out.println("Score is: " + user.getMyScore() + " : " + enemy.getEnemyScore());
+                            break;
+                        }
+                        case "3": {
+                            System.out.println("You picked Rock");
+                            game.subtractRound();
+                            switch (enemy.getEnemyMove()) {
+                                case "3":
+                                    System.out.println("Enemy choice is: Rock");
+                                    user.addMyScore();
+                                    enemy.addEnemyScore();
+                                    break;
+                                case "2":
+                                    System.out.println("Enemy choice is Paper ");
+                                    enemy.addEnemyScore();
+                                    break;
+                                case "1":
+                                    System.out.println("Enemy choice is Scissors");
+                                    user.addMyScore();
+                                    break;
+                            }
+                            System.out.println("Score is user " + user.getMyScore() + " : " + enemy.getEnemyScore() + " enemy");
+                            break;
+                        }
+                    }
+
+                    if (game.getRounds() == 0) {
+                        if (user.getMyScore() > enemy.getEnemyScore()) {
+                            System.out.println("!!! Congratulations you are the winner !!! ");
+                        } else if (user.getMyScore() < enemy.getEnemyScore()) {
+                            System.out.println("The winner is a " + enemy.getEnemyNick());
                         } else {
-                            System.out.println("Wrong option");
+                            System.out.println("It is a draw.");
                         }
-                        break;
                     }
-                    case "n": {
-                        System.out.println("Do you really want to restart the game ? \n Press y-Yes or n-No");
-                        String decision = br.readLine();
-                        if (decision.equals("n")) {
-                            System.out.println("Continue playing the game");
-                        } else if (decision.equals("y")) {
-                            game.restartRounds();
-                        } else {
-                            System.out.println("Wrong option");
-                        }
-                        break;
-                    }
-                    case "1": {
-                        System.out.println("You picked Scissors");
-                        game.subtractRound();
-                        switch (enemy.getEnemyMove()) {
-                            case "1":
-                                System.out.println("Enemy choice is: Scissors");
-                                user.addMyScore();
-                                enemy.addEnemyScore();
-                                break;
-                            case "2":
-                                System.out.println("Enemy choice is: Paper");
-                                user.addMyScore();
-                                break;
-                            case "3":
-                                System.out.println("Enemy choice is: Rock");
-                                enemy.addEnemyScore();
-                                break;
-                        }
-                        System.out.println("Score is: " + user.getMyScore() + " : " + enemy.getEnemyScore());
-                        break;
-                    }
-                    case "2": {
-                        System.out.println("You picked Paper");
-                        game.subtractRound();
-                        switch (enemy.getEnemyMove()) {
-                            case "2":
-                                System.out.println("Enemy choice is Paper");
-                                enemy.addEnemyScore();
-                                user.addMyScore();
-                                break;
-                            case "1":
-                                System.out.println("Enemy choice is Scissors");
-                                enemy.addEnemyScore();
-                                break;
-                            case "3":
-                                System.out.println("Enemy choice is Rock");
-                                user.addMyScore();
-                                break;
-                        }
-                        System.out.println("Score is: " + user.getMyScore() + " : " + enemy.getEnemyScore());
-                        break;
-                    }
-                    case "3": {
-                        System.out.println("You picked Rock");
-                        game.subtractRound();
-                        switch (enemy.getEnemyMove()) {
-                            case "3":
-                                System.out.println("Enemy choice is: Rock");
-                                user.addMyScore();
-                                enemy.addEnemyScore();
-                                break;
-                            case "2":
-                                System.out.println("Enemy choice is Paper ");
-                                enemy.addEnemyScore();
-                                break;
-                            case "1":
-                                System.out.println("Enemy choice is Scissors");
-                                user.addMyScore();
-                                break;
-                        }
-                        System.out.println("Score is user " + user.getMyScore() + " : " + enemy.getEnemyScore() + " enemy");
-                        break;
-                    }
+                } else {
+                    System.out.println("Wrong move");
                 }
-                if (game.getRounds() == 0) {
-                    if (user.getMyScore() > enemy.getEnemyScore()) {
-                        System.out.println("!!! Congratulations you are the winner !!! ");
-                    } else if (user.getMyScore() < enemy.getEnemyScore()) {
-                        System.out.println("The winner is a " + enemy.getEnemyNick());
-                    } else {
-                        System.out.println("It is a draw.");
-                    }
-                }
-                System.out.println("Starting a new game.");
             } while (game.getRounds() != 0);
+            System.out.println("Starting a new game.");
         }
     }
 }
