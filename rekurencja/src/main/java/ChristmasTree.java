4cd044c0127f84728cafacba8fcaff7e320d5bcb
leetdco2@gmail.com <leetdco2@gmail.com>
2020-10-30 16:42:56 +0100

    2.1 - writing good rows in bad position

--- rekurencja/src/main/java/ChristmasTree.java
+++ rekurencja/src/main/java/ChristmasTree.java
@@ -1,16 +1,26 @@
 public class ChristmasTree {
-    public String draw(String number){
-        int tree = Integer.parseInt(number);
 
-        return (" ");
+    public String drawTree(int number){
+        if(number == 0){
+            return "";
+        }else {
 
-
-
+            System.out.print("/");
+            for (int i=0;i<(2*(number-2));i++) {
+                if ( i == (2*(number-2))/2 )
+                {
+                    System.out.print("|");
+                }
+                System.out.print("_");
+            }
+            System.out.println("\\");
+            return drawTree(number-1);
+        }
     }
+
     public static void main(String[] args) {
         ChristmasTree christmasTree = new ChristmasTree();
-        String result = christmasTree.draw("6");
-
-        System.out.println(result);
+        String result = christmasTree.drawTree(6);
+        System.out.print(result);
     }
 }
